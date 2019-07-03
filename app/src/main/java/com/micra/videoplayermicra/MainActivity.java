a271c8c510489b943fd6a6f7606322abc2978149
Jigar Rangani <jigar.rangani1@gmail.com>
2019-07-03 10:57:50 +0530

    -add file provider,databinding ,sdp demon,and basic setUp

--- app/src/main/java/com/micra/videoplayermicra/MainActivity.java
+++ app/src/main/java/com/micra/videoplayermicra/MainActivity.java
@@ -1,14 +1,24 @@
 package com.micra.videoplayermicra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.micra.videoplayermicra.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
